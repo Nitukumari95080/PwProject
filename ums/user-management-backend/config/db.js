c54fbe9b65f82e26c18e54b37e20cbe8374f9d0f
NituKumari95080 <nitukumari251001@gmail.com>
2024-08-15 02:25:04 +0530

    node js express js and mogodb

--- ums/user-management-backend/config/db.js
+++ ums/user-management-backend/config/db.js
@@ -1,11 +1,13 @@
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI); // No need for options
-        console.log('MongoDB Connected...');
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected');
     } catch (err) {
-        console.error('MongoDB connection error:', err.message);
+        console.error(err.message);
         process.exit(1);
     }
 };
